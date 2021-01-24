9b0897b5a296f29621d2e47ee377b0d35e20a54d
Mako0530 <zhongtiancheng79@gmail.com>
2021-01-25 00:10:26 +0900

    マイグレーションファイル訂正、enumの実装

--- db/schema.rb
+++ db/schema.rb
@@ -24,7 +24,7 @@ ActiveRecord::Schema.define(version: 2021_01_24_145135) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
-    t.integer "gender"
+    t.integer "gender", default: 0, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,7 +5,7 @@
 #  id                     :bigint           not null, primary key
 #  email                  :string           default(""), not null
 #  encrypted_password     :string           default(""), not null
-#  gender                 :integer
+#  gender                 :integer          default(0), not null
 #  name                   :string
 #  remember_created_at    :datetime
 #  reset_password_sent_at :datetime
