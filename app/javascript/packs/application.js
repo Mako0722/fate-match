6e6751cc4db618a4352afa653d8b8f91b6f7ca81
Mako0530 <zhongtiancheng79@gmail.com>
2021-01-24 23:24:13 +0900

    トップページの作成。スマホ表示の設定

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,4 @@
-mport 'bootstrap';
+import 'bootstrap';
 import '../stylesheets/application';
 
 
@@ -11,6 +11,8 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+// require("jquery")
+// require('bootstrap/dist/js/bootstrap.min.js')
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
