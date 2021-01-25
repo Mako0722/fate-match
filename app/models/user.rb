92fb500dc3b9168aa2cb57a52766cacd0e65ee34
Mako0530 <zhongtiancheng79@gmail.com>
2021-01-25 20:47:07 +0900

    ルーティングの設定、コントローラーを作成、Font　Awesomeの導入、Navbarの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,7 @@
 #  remember_created_at    :datetime
 #  reset_password_sent_at :datetime
 #  reset_password_token   :string
+#  self_introduction      :text
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
 #
@@ -25,4 +26,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
          
   validates :name, presence: true
+
+  validates :self_introduction, length: { maximum: 500 }
+
+  enum gender: { man: 0, woman: 1 }
 end
