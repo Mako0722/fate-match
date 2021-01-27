11df0b39e695cc1d9a8c6b3cba827b1a58041389
Mako0530 <zhongtiancheng79@gmail.com>
2021-01-27 18:47:29 +0900

    チャットルームの作成機能追加

---
+++ app/models/chat_room.rb
@@ -0,0 +1,10 @@
+# == Schema Information
+#
+# Table name: chat_rooms
+#
+#  id         :bigint           not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class ChatRoom < ApplicationRecord
+end
